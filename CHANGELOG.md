cf4426f32eff836ef8733d338dc9720f02babf87
ctf0 <muah003@gmail.com>
2020-12-08 14:56:09 +0200

    003

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,3 +7,7 @@
 ## 0.0.2
 
 - add option to exclude text before first match & after last match
+
+## 0.0.3
+
+- fix package settings name
--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 const vscode = require('vscode')
 const escapeStringRegexp = require('escape-string-regexp')
-const PACKAGE_NAME = 'inverse_search'
+const PACKAGE_NAME = 'inverseSearch'
 
 let config = {}
 
@@ -16,63 +16,64 @@ async function activate(context) {
         }
     })
 
-    context.subscriptions.push(
-        vscode.commands.registerCommand('inverse.search', async () => {
-            let searchFor = await vscode.window.showInputBox({
-                placeHolder: 'ignore...',
-                value: await vscode.env.clipboard.readText() || '',
-                validateInput(v) {
-                    if (!v) {
-                        return 'you have to add something'
-                    } else {
-                        return ''
-                    }
-                }
-            })
+    context.subscriptions.push(vscode.commands.registerCommand('inverse.search', doStuff))
+}
 
-            let editor = vscode.window.activeTextEditor
-
-            if (searchFor && editor) {
-                let { document } = editor
-                let text = document.getText()
-                let arr = []
-                let prev = new vscode.Position(0, 0)
-
-                text.replace(new RegExp(escapeStringRegexp(searchFor), 'gm'), (match, offset) => {
-                    arr.push(
-                        new vscode.Selection(
-                            prev,
-                            document.positionAt(offset)
-                        )
-                    )
-
-                    prev = document.positionAt(offset + match.length)
-                })
-
-                // end of document
-                arr.push(
-                    new vscode.Selection(
-                        prev,
-                        new vscode.Position(document.lineCount, text.length)
-                    )
-                )
-
-                if (!config.includeStart) {
-                    arr.shift()
-                }
-
-                if (!config.includeEnd) {
-                    arr.pop()
-                }
-
-                if (arr.length) {
-                    return editor.selections = arr
-                } else {
-                    vscode.window.showInformationMessage('Inverse Search: Nothing Found !')
-                }
+async function doStuff(e) {
+    let searchFor = await vscode.window.showInputBox({
+        placeHolder : 'ignore...',
+        value       : await vscode.env.clipboard.readText() || '',
+        validateInput(v) {
+            if (!v) {
+                return 'you have to add something'
+            } else {
+                return ''
             }
+        }
+    })
+
+    let editor = vscode.window.activeTextEditor
+
+    if (searchFor && editor) {
+        let {document} = editor
+        let text = document.getText()
+        let arr = []
+        let prev = new vscode.Position(0, 0)
+        let regex = new RegExp(escapeStringRegexp(searchFor), 'gm')
+
+        text.replace(regex, (match, offset) => {
+            arr.push(
+                new vscode.Selection(
+                    prev,
+                    document.positionAt(offset)
+                )
+            )
+
+            prev = document.positionAt(offset + match.length)
         })
-    )
+
+        // end of document
+        arr.push(
+            new vscode.Selection(
+                prev,
+                new vscode.Position(document.lineCount, text.length)
+            )
+        )
+
+        if (!config.includeStart) {
+            arr.shift()
+        }
+
+        if (!config.includeEnd) {
+            arr.pop()
+        }
+
+        if (arr.length) {
+            return editor.selections = arr
+        } else {
+            vscode.window.showInformationMessage('Inverse Search: Nothing Found !')
+        }
+    }
 }
 
 async function readConfig() {
